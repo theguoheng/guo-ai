594d4ca318bdaab6bd0530a65157107984c0f16d
Guo Heng <guohengchin@gmail.com>
2016-06-06 23:00:04 +0800

    AJAX testing done. It works.

--- public/scripts/witapi.js
+++ public/scripts/witapi.js
@@ -1,4 +1,4 @@
-const Wit = require('node-wit').Wit;
+var wittyanswer = null;
 
 // Triggers upon submit button click. 
 $('#ask').on('click', function(e) {
@@ -6,4 +6,20 @@ $('#ask').on('click', function(e) {
 		var textInput = document.getElementById("answer");
 		// Test that it's found. 
 		console.log(textInput.value);
+		// JSONP request. 
+		$.ajax({
+  			url: 'https://api.wit.ai/message',
+  			data: {
+    			'q': 'set an alarm in 10min',
+    			'access_token' : 'OIMWEOE6BX6YJKLJRWZNZVWSVZJ6GERK'
+  			},
+  			dataType: 'jsonp',
+        async: false,
+  			method: 'GET',
+  			success: function(response) {
+      			console.log("success!", response);
+            wittyanswer = response; 
+ 			  }
+  	 });
+
 });
